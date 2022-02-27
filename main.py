c1799ff46cddebb76717fc088fdb33ebfcc763b0
zhangyu <zhang.yu2327@chinaredstar.com>
2022-02-27 11:09:32 +0800

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+def main():
+    session = requests.Session()
+    session.headers.update({
+        "authority": "easytoyou.eu",
+        "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "cache-control": "max-age=0",
+        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "macOS",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36"
+    })
+    resp = session.get('https://easytoyou.eu/login')
+
+
+if __name__ == '__main__':
+    main()
